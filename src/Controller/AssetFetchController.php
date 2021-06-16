534ff3c15a2d7c9adb384f1bcfd1d3f944cab7e1
Nathanael Dewhurst <nathanael@orientier.com>
2021-06-16 14:44:43 -0400

    Asset fetch handler draft. BF Image media source plugin. Permissions, etc.

---
+++ src/Controller/AssetFetchController.php
@@ -0,0 +1,201 @@
+<?php
+
+namespace Drupal\brandfolder\Controller;
+
+use Drupal\Core\Ajax\AjaxResponse;
+//use Drupal\Core\Ajax\AppendCommand;
+use Drupal\Core\Controller\ControllerBase;
+//use Drupal\Core\Url;
+//use Drupal\examples\Utility\DescriptionTemplateTrait;
+//use Symfony\Component\HttpFoundation\Response;
+
+/**
+ * Controller routines for AJAX example routes.
+ */
+class AssetFetchController extends ControllerBase {
+
+  /**
+   * {@inheritdoc}
+   */
+  protected function getModuleName() {
+    return 'brandfolder';
+  }
+
+  /**
+   * Fetch assets applicable to the current context.
+   */
+  public function fetchAssetsCallback($context_id) {
+
+    // @todo: Configurable passphrase or generate once per install and keep behind the scenes, etc.
+    $context = openssl_decrypt($context_id, 'aes-256-gcm', 'brandfolder_encrypt');
+
+    $response = new AjaxResponse();
+
+    $params = \Drupal::request()->query->all();
+
+    $bf_params = [
+      'fields' => 'cdn_url',
+      'include' => 'custom_fields',
+    ];
+    $search = '*';
+    if (!empty($params['search'])) {
+      $search = $params['search'];
+    }
+    // If we're in an image field context, load the field/instance and see if
+    // there are any relevant restrictions we should incorporate into our search.
+    if (isset($context['#field_name'])) {
+      // @todo: media type BF config, etc.
+//      if ($instance = field_read_instance($context['#entity_type'], $context['#field_name'], $context['#bundle'])) {
+//        if (!empty($instance['settings']['file_extensions'])) {
+//          $file_extensions = $instance['settings']['file_extensions'];
+//        }
+//        if (!empty($instance['settings']['min_resolution'])) {
+//          $min_res = $instance['settings']['min_resolution'];
+//        }
+//        if (!empty($instance['settings']['max_resolution'])) {
+//          $max_res = $instance['settings']['max_resolution'];
+//        }
+//      }
+    }
+    else {
+      if (!empty($params['allowed_file_extensions'])) {
+        $file_extensions = $params['allowed_file_extensions'];
+      }
+      // @todo: Determine whether this is/will be supported by Brandfolder API.
+      // phpcs:disable Drupal.Commenting.InlineComment
+      // if (!empty($params['max_file_size'])) {
+      //   $max_file_size = $params['max_file_size'];
+      // }
+      // phpcs:enable
+      if (!empty($params['min_resolution'])) {
+        $min_res = $params['min_resolution'];
+      }
+      if (!empty($params['max_resolution'])) {
+        $max_res = $params['max_resolution'];
+      }
+    }
+
+    if (isset($file_extensions)) {
+      $search .= ' AND extensions:(' . $file_extensions . ')';
+    }
+    if (isset($min_res)) {
+      $min_res = explode('x', $min_res);
+      if (count($min_res) == 2) {
+        [$min_width, $min_height] = $min_res;
+        if (is_numeric($min_width)) {
+          $search .= " AND width:>=$min_width";
+        }
+        if (is_numeric($min_height)) {
+          $search .= " AND height:>=$min_height";
+        }
+      }
+    }
+    if (isset($max_res)) {
+      $max_res = explode('x', $max_res);
+      if (count($max_res) == 2) {
+        [$max_width, $max_height] = $max_res;
+        if (is_numeric($max_width)) {
+          $search .= " AND width:<=$max_width";
+        }
+        if (is_numeric($max_height)) {
+          $search .= " AND height:<=$max_height";
+        }
+      }
+    }
+
+    if (isset($params['sort'])) {
+      $sort = $params['sort'];
+      // @todo: Consider moving this sort of validation into the Brandfolder PHP API library.
+      $valid_sort_criteria = [
+        'name',
+        'score',
+        'position',
+        'updated_at',
+        'created_at',
+      ];
+      if (in_array($sort, $valid_sort_criteria)) {
+        $bf_params['sort_by'] = $sort;
+      }
+    }
+    if (isset($params['order'])) {
+      $bf_params['order'] = $params['order'];
+    }
+    if (isset($params['orientation'])) {
+      $aspect_values = explode(',', $params['orientation']);
+      if (count($aspect_values) > 0) {
+        $search .= ' AND (';
+        $search .= implode(' OR ', array_map(function ($aspect) {
+          return "aspect:$aspect";
+        }, $aspect_values));
+        $search .= ')';
+      }
+    }
+    if (isset($params['created_at'])) {
+      $search .= ' AND created_at:>now-' . $params['created_at'];
+    }
+    // @todo: Add date_updated?
+    if (isset($params['tags'])) {
+      $quoted_tags = array_map(function ($tag_name) {
+        return "\"$tag_name\"";
+      }, explode(',', $params['tags']));
+      $search .= ' AND tags:(' . implode(' ', $quoted_tags) . ')';
+    }
+
+    // @todo: Consider putting this in an array or some other format that would be more cleanly altered by other modules.
+    $baseline_search_criteria = 'approved:true AND expired:false AND unpublished:false';
+
+    if (strlen($search) > 0) {
+      $bf_params['search'] = "$search AND $baseline_search_criteria";
+    }
+    else {
+      $bf_params['search'] = $baseline_search_criteria;
+    }
+
+    // Allow other modules to alter the query.
+    $collection = NULL;
+    // @todo
+//    drupal_alter('brandfolder_get_assets', $bf_params, $collection, $context);
+
+    $bf = brandfolder_api();
+    $assets = $bf->listAssets($bf_params, $collection);
+
+    if (is_array($assets->data)) {
+      $thumbnails = array_map(function ($asset) {
+        // Use an alternate file for GIF images so that animated GIFs will appear
+        // animated in the browser preview (element.gif is a still image).
+        $thumbnail_url = str_replace('/element.gif', '/view.gif', $asset->attributes->thumbnail_url);
+        $alt_text = isset($asset->custom_field_values['alt-text']) ? $asset->custom_field_values['alt-text'] : '';
+        $cdn_url = $asset->attributes->cdn_url;
+
+//        return '<div class="brandfolder-asset" data-bf-asset-id="' . $asset->id . '"><img src="' . $thumbnail_url . '" class="brandfolder-asset__image" data-bf-asset-id="' . $asset->id . '" data-bf-asset-cdn-url="' . $cdn_url . '" alt="' . $alt_text . '"></div>';
+
+        $markup = "<li class=\"brandfolder-asset\" data-bf-asset-id=\"{$asset->id}\">"
+          .   "<figure>"
+          .     '<img src="' . $thumbnail_url . '" class="brandfolder-asset__image" data-bf-asset-id="' . $asset->id . '" data-bf-asset-cdn-url="' . $cdn_url . '" alt="' . $alt_text . '">'
+          .     "<figcaption>"
+          .       "<div class=\"bf-asset-metadata\"><strong>Name:</strong> {$asset->attributes->name}</div>"
+          .       "<div class=\"bf-asset-metadata\"><strong>Asset ID:</strong> {$asset->id}</div>"
+          .     "</figcaption>"
+          .   "</figure>"
+          . "</li>";
+
+        return $markup;
+
+      }, $assets->data);
+      $content = count($assets->data) > 0 ? implode(' ', $thumbnails) : t('No images found.');
+      $output = [
+        'content' => $content,
+      ];
+
+      // @todo Offer an alternate response format with assets as JSON objects instead of HTML.
+    }
+    else {
+      $output = [];
+    }
+
+    $response->setData($output);
+
+    return $response;
+  }
+
+}
---
+++ src/Plugin/media/Source/BrandfolderImage.php
@@ -0,0 +1,286 @@
+<?php
+
+namespace Drupal\brandfolder\Plugin\media\Source;
+
+use Drupal\Component\Datetime\TimeInterface;
+use Drupal\Core\Cache\CacheBackendInterface;
+use Drupal\Core\Config\ConfigFactoryInterface;
+use Drupal\Core\Entity\EntityFieldManagerInterface;
+use Drupal\Core\Entity\EntityTypeManagerInterface;
+use Drupal\Core\Extension\ModuleHandlerInterface;
+use Drupal\Core\Field\FieldTypePluginManagerInterface;
+use Drupal\Core\Form\FormStateInterface;
+use Drupal\Core\Logger\LoggerChannelFactoryInterface;
+use Drupal\Core\Routing\UrlGeneratorInterface;
+use Drupal\Core\Session\AccountProxyInterface;
+use Drupal\media\MediaInterface;
+use Drupal\media\MediaSourceBase;
+use Drupal\media\MediaTypeInterface;
+use Symfony\Component\DependencyInjection\ContainerInterface;
+
+/**
+ * Allows Brandfolder image assets to be used by Drupal's Media system.
+ *
+ * @todo: generic BrandfolderAsset class and various default type-based classes (image, video, etc.)
+ *
+ * @MediaSource(
+ *   id = "brandfolder_image",
+ *   label = @Translation("Brandfolder Image"),
+ *   description = @Translation("Allows Brandfolder assets to be used by Drupal's Media system."),
+ *   allowed_field_types = {"string", "image"}
+ * )
+ */
+class BrandfolderImage extends MediaSourceBase {
+
+  /**
+   * Brandfolder API.
+   *
+   * @var \Brandfolder\Brandfolder
+   *   BF SDK.
+   */
+  protected $brandfolderClient;
+
+  /**
+   * Account proxy.
+   *
+   * @var \Drupal\Core\Session\AccountProxyInterface
+   */
+  protected $accountProxy;
+
+  /**
+   * The url generator.
+   *
+   * @var \Drupal\Core\Routing\UrlGeneratorInterface
+   */
+  protected $urlGenerator;
+
+  /**
+   * Statically cached metadata information for the given assets.
+   *
+   * @var array
+   */
+  protected $metadata;
+
+  /**
+   * The logger factory service.
+   *
+   * @var \Drupal\Core\Logger\LoggerChannelFactoryInterface
+   */
+  protected $logger;
+
+  /**
+   * The cache service.
+   *
+   * @var \Drupal\Core\Cache\CacheBackendInterface
+   */
+  protected $cache;
+
+  /**
+   * The time service.
+   *
+   * @var \Drupal\Component\Datetime\TimeInterface
+   */
+  protected $time;
+
+  /**
+   * The module handler.
+   *
+   * @var \Drupal\Core\Extension\ModuleHandlerInterface
+   */
+  protected $moduleHandler;
+
+  /**
+   * Constructs a new class instance.
+   *
+   * @param array $configuration
+   *   A configuration array containing information about the plugin instance.
+   * @param string $plugin_id
+   *   The plugin_id for the plugin instance.
+   * @param mixed $plugin_definition
+   *   The plugin implementation definition.
+   * @param \Drupal\Core\Entity\EntityTypeManagerInterface $entity_type_manager
+   *   Entity type manager service.
+   * @param \Drupal\Core\Entity\EntityFieldManagerInterface $entity_field_manager
+   *   Entity field manager service.
+   * @param \Drupal\Core\Field\FieldTypePluginManagerInterface $field_type_manager
+   *   The field type plugin manager service.
+   * @param \Drupal\Core\Config\ConfigFactoryInterface $config_factory
+   *   The config factory service.
+   * @param \Drupal\Core\Session\AccountProxyInterface $account_proxy
+   *   Account proxy.
+   * @param \Drupal\Core\Routing\UrlGeneratorInterface $url_generator
+   *   The url generator service.
+   * @param \Drupal\Core\Logger\LoggerChannelFactoryInterface $logger
+   *   The logger factory service.
+   * @param \Drupal\Core\Cache\CacheBackendInterface $cache
+   *   The cache service.
+   * @param \Drupal\Core\Extension\ModuleHandlerInterface $module_handler
+   *   The module handler.
+   */
+  public function __construct(array $configuration, $plugin_id, $plugin_definition, EntityTypeManagerInterface $entity_type_manager, EntityFieldManagerInterface $entity_field_manager, FieldTypePluginManagerInterface $field_type_manager, ConfigFactoryInterface $config_factory, AccountProxyInterface $account_proxy, UrlGeneratorInterface $url_generator, LoggerChannelFactoryInterface $logger, CacheBackendInterface $cache, TimeInterface $time, ModuleHandlerInterface $module_handler) {
+    parent::__construct($configuration, $plugin_id, $plugin_definition, $entity_type_manager, $entity_field_manager, $field_type_manager, $config_factory);
+
+    // @todo: DI
+    $this->brandfolderClient = brandfolder_api();
+    $this->accountProxy = $account_proxy;
+    $this->urlGenerator = $url_generator;
+    $this->logger = $logger;
+    $this->cache = $cache;
+    $this->time = $time;
+    $this->moduleHandler = $module_handler;
+  }
+
+  /**
+   * {@inheritdoc}
+   */
+  public static function create(ContainerInterface $container, array $configuration, $plugin_id, $plugin_definition) {
+    return new static(
+      $configuration,
+      $plugin_id,
+      $plugin_definition,
+      $container->get('entity_type.manager'),
+      $container->get('entity_field.manager'),
+      $container->get('plugin.manager.field.field_type'),
+      $container->get('config.factory'),
+      $container->get('current_user'),
+      $container->get('url_generator'),
+      $container->get('logger.factory'),
+      $container->get('cache.data'),
+      $container->get('datetime.time'),
+      $container->get('module_handler')
+    );
+  }
+
+  /**
+   * {@inheritdoc}
+   */
+  public function getMetadataAttributes() {
+    $fields = [
+      'asset_id' => $this->t('Asset ID'),
+      'name' => $this->t('Name'),
+      'description' => $this->t('Description'),
+      'type' => $this->t('Type'),
+      'thumbnail_url' => $this->t('Thumbnail url'),
+      'width' => $this->t('Width'),
+      'height' => $this->t('Height'),
+      'created' => $this->t('Date created'),
+      'updated' => $this->t('Date updated'),
+      'tags' => $this->t('Tags'),
+      // @todo: Custom BF fields, including semi-required fields such as alt-text.
+    ];
+
+    return $fields;
+  }
+
+  /**
+   * {@inheritdoc}
+   */
+  public function defaultConfiguration() {
+    return [
+      'source_field' => 'field_brandfolder_asset_id',
+    ];
+  }
+
+  /**
+   * {@inheritdoc}
+   */
+  public function getMetadata(MediaInterface $media, $name) {
+    $bf_asset_id = $this->getSourceFieldValue($media);
+    if ($name == 'bf_asset_id') {
+      return $bf_asset_id;
+    }
+
+    switch ($name) {
+      case 'thumbnail_url':
+        // @todo: store in local metadata cache/store. Look into "sig" (signature) URL param lifespan and expiry param, etc.
+//          $thumbnail_url = "https://thumbs.bfldr.com/as/$bf_asset_id?expiry=1624467346&fit=bounds&height=162&sig=ZTY3ZDA5MTFjMWQxMmQ1Yjk5ZjJjYTg3OTczZGYxZDgzODYwZWQ3Yw%3D%3D&width=262";
+        // Temp.
+        $cdn_thumb_url = "https://cdn.bfldr.com/U91JRM9I/as/$bf_asset_id/bf-drupal-media-thumb--$bf_asset_id.jpg?width=262&height=162&fit=bounds";
+
+        return $cdn_thumb_url;
+
+//        case 'created':
+//          return isset($this->metadata[$bf_asset_id]['dateCreated']) ? $this->metadata[$bf_asset_id]['dateCreated'] : FALSE;
+//
+//        case 'modified':
+//          return isset($this->metadata[$bf_asset_id]['dateModified']) ? $this->metadata[$bf_asset_id]['dateModified'] : FALSE;
+
+      case 'default_name':
+//          return isset($this->metadata[$bf_asset_id]['name']) ? $this->metadata[$bf_asset_id]['name'] : parent::getMetadata($media, 'default_name');
+        // @todo
+        return "Brandfolder Asset $bf_asset_id";
+
+//        default:
+//          return isset($this->metadata[$bf_asset_id][$name]) ? $this->metadata[$bf_asset_id][$name] : FALSE;
+    }
+
+    return FALSE;
+  }
+
+  /**
+   * {@inheritdoc}
+   */
+  public function buildConfigurationForm(array $form, FormStateInterface $form_state) {
+    try {
+      $this->brandfolderClient->listAssets();
+    }
+    catch (\Exception $exception) {
+      if ($this->accountProxy->hasPermission('administer brandfolder settings')) {
+        $this->messenger()->addError($this->t('Could not connect to Brandfolder. Make sure the connection is <a href=":url">configured</a> properly.', [
+          ':url' => $this->urlGenerator->generateFromRoute('brandfolder.brandfolder_settings_form'),
+        ]));
+      }
+      else {
+        $this->messenger()->addError($this->t('Something went wrong with the Brandfolder connection. Please contact the site administrator.'));
+      }
+    }
+
+    // @todo: config such as allowed collections, etc.
+
+    return parent::buildConfigurationForm($form, $form_state);
+  }
+
+  /**
+   * Creates the image field storage definition.
+   *
+   * @return \Drupal\field\FieldStorageConfigInterface
+   *   The unsaved field storage definition.
+   */
+  public function createImageFieldStorage() {
+    return $this->entityTypeManager->getStorage('field_storage_config')
+      ->create([
+        'entity_type' => 'media',
+        'field_name' => 'bf_image',
+        'type' => 'image',
+        'cardinality' => 1,
+        'locked' => TRUE,
+        'field_settings' => [
+          'uri_scheme' => 'bf',
+          'target_type' => 'file'
+        ],
+      ]);
+  }
+
+  /**
+   * Creates the image field definition.
+   *
+   * @param \Drupal\media\MediaTypeInterface $type
+   *   The media type.
+   *
+   * @return \Drupal\field\FieldConfigInterface
+   *   The unsaved field definition. The field storage definition, if new,
+   *   should also be unsaved.
+   */
+  public function createImageField(MediaTypeInterface $type) {
+    return $this->entityTypeManager->getStorage('field_config')
+      ->create([
+        'entity_type' => 'media',
+        'field_name' => 'bf_image',
+        'bundle' => $type->id(),
+        'label' => 'Brandfolder Image',
+        'translatable' => FALSE,
+        'field_type' => 'image',
+      ]);
+  }
+
+}
